3bd1a80d1041c7d17b9919ffb360043f985780db
Roshni Santosh Gupta <84021865+roshni99679@users.noreply.github.com>
2022-08-13 18:01:20 +0530

    Update KadanesAlgo.py

--- Array/KadanesAlgo.py
+++ Array/KadanesAlgo.py
@@ -1,23 +1,25 @@
-def maxSubArraySum(arr,N):
-        ##Your code here
-        for i in range(1,N):
-            if arr[i-1]>0:
-                arr[i]+=arr[i-1]
-        return max(arr)
-arr=[5,-4,-2,6,-1]
-print(maxSubArraySum(arr,len(arr)))
-#6
-#Runs even if all ele are negative 
+def maxSubArray(nums):
+    c_sum = 0
+    max_sum = -1e8
+    for i in range(len(nums)):
+        c_sum += nums[i]
+        if c_sum > max_sum:
+            max_sum = c_sum
+        if c_sum < 0:
+            c_sum = 0
+    return max_sum
 
 def maxsumsubarr(arr):
-    n=len(arr)
-    for i in range(1,n):
-        if arr[i-1]>0:
-            arr[i]+=arr[i-1]
+    n = len(arr)
+    for i in range(1, n):
+        if arr[i-1] > 0:
+            arr[i] += arr[i-1]
             # print(arr[i],end=" ")     #1 -1 5
     # print(arr)  #[5, 1, -1, 6, 5]
     return max(arr)
-arr=[5,-4,-2,6,-1]
+
+
+arr = [5, -4, -2, 6, -1]
 print(maxsumsubarr(arr))
-#6
+# 6
 
